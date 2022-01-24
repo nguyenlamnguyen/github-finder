084794e893b4d8eee0d9f6c096399744c8a4ee7f
Nguyen Lam Nguyen <infernonguyen118@gmail.com>
2022-01-24 19:47:29 +0700

    Update

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,28 +9,80 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
-        loading: false
+        user: {},
+        loading: false,
     }
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
     // Get initial users (testing purposes)
-    const fetchUsers = async () => {
+    // const fetchUsers = async () => {
+    //     setLoading()
+
+    //     const response = await fetch(`${GITHUB_URL}/users`,
+    //     {
+    //         headers: {
+    //             Authorization: `token ${GITHUB_TOKEN}`
+    //         }
+    //     })
+  
+    //     const data = await response.json()
+  
+    //     dispatch({
+    //         type: 'GET_USERS',
+    //         payload: data,
+    //     })
+    // }
+
+    // Get search results
+    const searchUsers = async (text) => {
         setLoading()
 
-        const response = await fetch(`${GITHUB_URL}/users`,
+        const params = new URLSearchParams({
+            q: text
+        })
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`,
         {
             headers: {
                 Authorization: `token ${GITHUB_TOKEN}`
             }
         })
   
-        const data = await response.json()
+        const {items} = await response.json()
   
         dispatch({
             type: 'GET_USERS',
-            payload: data,
+            payload: items,
         })
+    }
+
+    // Get single user
+    const getUser = async (login) => {
+        setLoading()
+        
+        const response = await fetch(`${GITHUB_URL}/users/${login}`,
+        {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+        
+        if(response.status === 404){
+            window.location = '/notfound'
+        } else {
+            const data = await response.json()
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            })
+        }
+
+    }
+
+    // Clear users result
+    const clearUsers = () => {
+        dispatch({type: 'CLEAR_USERS'})
     }
 
     // Set loading
@@ -40,8 +92,12 @@ export const GithubProvider = ({children}) => {
 
     return <GithubContext.Provider value={{
         users: state.users,
+        user: state.user,
         loading: state.loading,
-        fetchUsers
+        // fetchUsersm
+        searchUsers,
+        clearUsers,
+        getUser
     }}>
         {children}
     </GithubContext.Provider>
